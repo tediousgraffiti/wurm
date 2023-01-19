4fceafce368b5bbbc38ee97a5d110141c516c14b
tediousgraffiti <tediousgraffiti@gmail.com>
2023-01-19 11:16:31 -0600

    no walking back through self, collisions and gameover, factor out gameboard from app

--- wurm.py
+++ wurm.py
@@ -6,12 +6,15 @@ import logging
 
 
 # TODO:
-# 1. no walking back through self
-# 2. collision detection and gameover
-# 3. factor out App from GameBoard
+# 1. no walking back through self (x)
+# 2. collision detection and gameover (x)
+# 3. factor out App from GameBoard (x)
 # 4. fix hardcoded board dimensions
 # 5. Add scoring, hud
 # 6. add additional configuration options (e.g. tick speed, starting length, toggle walls) and CLI
+# 7. Bugsquash
+#    - what happens if the apple is placed inside the snake's body?
+#    - make sure apple is placed within display area?
 
 
 class Direction(Enum):
@@ -22,6 +25,11 @@ class Direction(Enum):
 
 
 class Snake:
+    OPPOSITE_DIRECTIONS = [
+        {Direction.UP, Direction.DOWN},
+        {Direction.LEFT, Direction.RIGHT},
+    ]
+
     def __init__(self, head_color: str = "red", body_color: str = "green"):
         self._stack = [(20, 20), (20, 21), (20, 22), (20, 23)]
         self.length = len(self._stack)  # this should be a property
@@ -32,6 +40,10 @@ class Snake:
     @property
     def head(self):
         return self._stack[0]
+
+    @property
+    def tail(self):
+        return self._stack[1:]
 
     def move(self):
         head = self._stack.pop(0)
@@ -51,7 +63,7 @@ class Snake:
         self._stack.pop()
 
     def extend(self):
-        # grow the snake by 1 cell
+        # grow the snake by 1 cell by just duping the end of the tail
         self._stack.append(tuple(self._stack[-1]))
 
     @property
@@ -59,8 +71,13 @@ class Snake:
         return self._direction
 
     @direction.setter
-    def direction(self, dir: Direction):
-        self._direction = dir
+    def direction(self, new_direction: Direction):
+        if {self.direction, new_direction} not in Snake.OPPOSITE_DIRECTIONS:
+            self._direction = new_direction
+
+    def is_self_intersecting(self):
+        # logging.warning(f"{self.head}, {self.tail}, {self.head in self.tail}")
+        return self.head in self.tail
 
 
 class Apple:
@@ -89,7 +106,8 @@ class GameState:
 
     @property
     def gameover(self):
-        return False
+        # logging.warning(f"{not self.snake.is_self_intersecting()}")
+        return self.snake.is_self_intersecting()
 
     @property
     def snake(self):
@@ -101,12 +119,11 @@ class GameState:
 
     def consume_apple(self):
         self._apple = Apple()
+        logging.warning(f"{self._apple.coordinates}")
         self.snake.extend()
 
 
 class GameBoard:
-    KEYS = ["w", "a", "s", "d", "Left", "Right", "Up", "Down"]
-
     def __init__(
         self, parent: tk.Widget, background: str = "black", tile_size: int = 10
     ):
@@ -120,53 +137,70 @@ class GameBoard:
             background=background,
         )
         self._canvas.pack()
-        self._key = None
-        self.state = GameState()
 
-    def set_key_event(self, event):
-        logging.warning(event)
-        if event.keysym in GameBoard.KEYS:
-            self._key = event.keysym
-
-    def render(self):
+    def render(self, state):
         # clear the canvas
         self._canvas.delete(tk.ALL)
 
         # draw the snake's head
-        head = self.state.snake._stack[0]
+        head = state.snake._stack[0]
         self._canvas.create_rectangle(
             head[0] * self.tile_size,
             head[1] * self.tile_size,
             head[0] * self.tile_size + self.tile_size,
             head[1] * self.tile_size + self.tile_size,
-            fill=self.state.snake.head_color,
+            fill=state.snake.head_color,
         )
 
         # draw the snake's body
-        for cell in self.state.snake._stack[1:]:
+        for cell in state.snake._stack[1:]:
             self._canvas.create_rectangle(
                 cell[0] * self.tile_size,
                 cell[1] * self.tile_size,
                 cell[0] * self.tile_size + self.tile_size,
                 cell[1] * self.tile_size + self.tile_size,
-                fill=self.state.snake.body_color,
+                fill=state.snake.body_color,
             )
 
         # draw the apple
         # print(self.state.apple.x, self.state.apple.y, self.state.apple.color)
         self._canvas.create_rectangle(
-            self.state.apple.x * self.tile_size,
-            self.state.apple.y * self.tile_size,
-            self.state.apple.x * self.tile_size + self.tile_size,
-            self.state.apple.y * self.tile_size + self.tile_size,
-            fill=self.state.apple.color,
+            state.apple.x * self.tile_size,
+            state.apple.y * self.tile_size,
+            state.apple.x * self.tile_size + self.tile_size,
+            state.apple.y * self.tile_size + self.tile_size,
+            fill=state.apple.color,
         )
 
-    def game_loop(self):
-        self._canvas.after(200, self.game_loop)
 
+class App(tk.Tk):
+    KEYS = ["w", "a", "s", "d", "Left", "Right", "Up", "Down"]
+
+    def __init__(
+        self,
+        screenName=None,
+        baseName=None,
+        className="Tk",
+        usetk=True,
+        sync=False,
+        use=None,
+    ):
+        tk.Tk.__init__(self, screenName, baseName, className, usetk, sync, use)
+        self._frame = tk.Frame(self)
+        self._frame.pack()
+        self.state = GameState()
+        self.board = GameBoard(self._frame)
+        self._key = None
+        self.bind("<Key>", self.set_key_event)
+
+    def set_key_event(self, event):
+        logging.warning(event)
+        if event.keysym in App.KEYS:
+            self._key = event.keysym
+
+    def game_loop(self):
         if not self.state.gameover:
-            self.render()
+            self.board.render(self.state)
             if self._key:
                 match self._key:
                     case "w" | "Up":
@@ -180,14 +214,22 @@ class GameBoard:
             self.state.snake.move()
             if self.state.snake.head == self.state.apple.coordinates:
                 self.state.consume_apple()
+            self.board._canvas.after(200, self.game_loop)
+        else:
+            logging.error("YOU DIED")
+
+    def mainloop(self, n=0):
+        self.game_loop()
+        tk.Tk.mainloop(self, n)
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    frame = tk.Frame(root)
-    frame.pack()
-    board = GameBoard(frame)
-    root.bind("<Key>", board.set_key_event)
-    board.game_loop()
-    root.mainloop()
+    # root = tk.Tk()
+    # frame = tk.Frame(root)
+    # frame.pack()
+    # board = GameBoard(frame)
+    # root.bind("<Key>", board.set_key_event)
+    # board.game_loop()
+    # root.mainloop()
+    App().mainloop()
 #
